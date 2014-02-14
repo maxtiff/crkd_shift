d4fd95a3f98af9f5ab33b10e4be87657ab809c69
maxtiff <travismay@gmail.com>
2014-02-13 19:24:17 -0600

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def submission
   end
+
+  def contact
+  end
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Crooked/Shift'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Crooked/Shift')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Crooked/Shift | Cover")
+	end
+  end
+
+   describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Crooked/Shift | About")
+	end
+  end
+
+    describe "Submission page" do
+
+    it "should have the content 'Submission'" do
+      visit '/static_pages/submission'
+      expect(page).to have_content('submission')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/submission'
+      expect(page).to have_title("Crooked/Shift | Submission")
+	end
+  end
+
+    describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Crooked/Shift | Help")
+	end
+  end
+
+    describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Crooked/Shift | Contact")
+	end
+  end
+end
